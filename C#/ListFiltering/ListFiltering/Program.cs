c25c9f5869ee5be34c99e3e40936d1d7c02b4ef8
Michael Schmitz <michael.schmitz@deepsphere.ch>
2022-11-27 23:08:29 +0100

    Solve C# Challange ListFiltering (kyu 7)

---
+++ C#/ListFiltering/ListFiltering/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class ListFilterer
+{
+    public static void Main(string[] args)
+    {
+        IEnumerable<int> listOfIntegers = GetIntegersFromList(new List<object> { 1, 2, 3, 'a', 'b' });
+        foreach (int i in listOfIntegers)
+        {
+            Console.WriteLine(i);
+        }
+    }
+    public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+    {
+        List<int> listOfIntegers = new List<int>();
+        foreach (object item in listOfItems)
+        {
+            if (item is int)
+            {
+                listOfIntegers.Add((int)item);
+            }
+        }
+        return listOfIntegers;
+    }
+}
